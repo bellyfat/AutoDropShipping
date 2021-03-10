be95fa437eba116702523b5172c53bae7671969e
ItamarGino <49710930+ItamarGino@users.noreply.github.com>
2021-03-10 14:00:01 +0200

    Create Constances.py

---
+++ Constances.py
@@ -0,0 +1,192 @@
+# All the constances
+
+################    Collection module  ################
+################    Amazon:
+url_amazon_ToysGames_AmazonExclusiveToys = 'https://www.amazon.com/s?rh=n%3A165793011%2Cn%3A%212334111011%2Cn%3A%212334173011%2Cn%3A%2120795333011%2Cn%3A17403333011&page=2&qid=1601968406&ref=lp_17403333011_pg_2'
+url_amazon_ToysGames_ArtsCrafts = 'https://www.amazon.com/s?k=arts+and+crafts&i=toys-and-games&page=2&pf_rd_i=165793011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e7b0592f-ffeb-48c4-9453-25c721806001&pf_rd_r=FECH07WR2FC8EZYQK48Q&pf_rd_s=merchandised-search-15&pf_rd_t=101&qid=1601968423&ref=sr_pg_2'
+url_amazon_ToysGames_BabyToddlerToys = 'https://www.amazon.com/s?k=Baby+%26+Toddler+Toys&i=toys-and-games&page=2&pf_rd_i=165793011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e7b0592f-ffeb-48c4-9453-25c721806001&pf_rd_r=FECH07WR2FC8EZYQK48Q&pf_rd_s=merchandised-search-15&pf_rd_t=101&qid=1601968437&ref=sr_pg_2'
+url_amazon_ToysGames_BuildingToys = 'https://www.amazon.com/s?k=building+toys&i=toys-and-games&page=2&pf_rd_i=165793011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e7b0592f-ffeb-48c4-9453-25c721806001&pf_rd_r=FECH07WR2FC8EZYQK48Q&pf_rd_s=merchandised-search-15&pf_rd_t=101&qid=1601968456&ref=sr_pg_2'
+url_amazon_ToysGames_Dolls = 'https://www.amazon.com/s?k=dolls&i=toys-and-games&page=2&pf_rd_i=165793011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e7b0592f-ffeb-48c4-9453-25c721806001&pf_rd_r=FECH07WR2FC8EZYQK48Q&pf_rd_s=merchandised-search-15&pf_rd_t=101&qid=1601968470&ref=sr_pg_2'
+url_amazon_DressUpPretendPlay = 'https://www.amazon.com/s?k=dress+up+and+pretend+play&i=toys-and-games&rh=n%3A166316011&page=2&pf_rd_i=165793011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e7b0592f-ffeb-48c4-9453-25c721806001&pf_rd_r=FECH07WR2FC8EZYQK48Q&pf_rd_s=merchandised-search-15&pf_rd_t=101&qid=1601968484&ref=sr_pg_2'
+url_amazon_Games = 'https://www.amazon.com/s?k=games&i=toys-and-games&page=2&pf_rd_i=165793011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e7b0592f-ffeb-48c4-9453-25c721806001&pf_rd_r=FECH07WR2FC8EZYQK48Q&pf_rd_s=merchandised-search-15&pf_rd_t=101&qid=1601968503&ref=sr_pg_2'
+url_amazon_vehicles = 'https://www.amazon.com/s?rh=n%3A165793011%2Cn%3A%21165795011%2Cn%3A276729011&page=2&qid=1601968535&ref=lp_276729011_pg_2'
+url_amazon_PreschoolToys = 'https://www.amazon.com/s?k=preschool+toys&i=toys-and-games&page=2&pf_rd_i=165793011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e7b0592f-ffeb-48c4-9453-25c721806001&pf_rd_r=FECH07WR2FC8EZYQK48Q&pf_rd_s=merchandised-search-15&pf_rd_t=101&qid=1601968551&ref=sr_pg_2'
+url_amazon_Puzzles = 'https://www.amazon.com/s?k=puzzles&i=toys-and-games&page=2&pf_rd_i=165793011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e7b0592f-ffeb-48c4-9453-25c721806001&pf_rd_r=FECH07WR2FC8EZYQK48Q&pf_rd_s=merchandised-search-15&pf_rd_t=101&qid=1601968575&ref=sr_pg_2'
+url_amazon_ToyCars = 'https://www.amazon.com/s?k=toy+cars&i=toys-and-games&page=2&pf_rd_i=165793011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e7b0592f-ffeb-48c4-9453-25c721806001&pf_rd_r=FECH07WR2FC8EZYQK48Q&pf_rd_s=merchandised-search-15&pf_rd_t=101&qid=1601968591&ref=sr_pg_2'
+url_amazon_Electronics = 'https://www.amazon.com/s?i=electronics-intl-ship&rh=n%3A%2116225009011&page=2&qid=1601968608&ref=lp_16225009011_pg_2'
+url_amazon_RideOns = 'https://www.amazon.com/s?k=ride+ons&i=toys-and-games&page=2&pf_rd_i=165793011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e7b0592f-ffeb-48c4-9453-25c721806001&pf_rd_r=C2PAXDMDEC994MG1EM89&pf_rd_s=merchandised-search-15&pf_rd_t=101&qid=1601968622&ref=sr_pg_2'
+url_amazon_SportsOutdoorPlay = 'https://www.amazon.com/s?k=sports+and+outdoor+play&i=toys-and-games&page=2&pf_rd_i=165793011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e7b0592f-ffeb-48c4-9453-25c721806001&pf_rd_r=C2PAXDMDEC994MG1EM89&pf_rd_s=merchandised-search-15&pf_rd_t=101&qid=1601968636&ref=sr_pg_2'
+url_amazon_SteamToys = 'https://www.amazon.com/s?k=steam+toys&i=toys-and-games&page=2&pf_rd_i=165793011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e7b0592f-ffeb-48c4-9453-25c721806001&pf_rd_r=C2PAXDMDEC994MG1EM89&pf_rd_s=merchandised-search-15&pf_rd_t=101&qid=1601968650&ref=sr_pg_2'
+url_amazon_StuffedAnimals = 'https://www.amazon.com/s?k=stuffed+animals&i=toys-and-games&page=2&pf_rd_i=165793011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e7b0592f-ffeb-48c4-9453-25c721806001&pf_rd_r=C2PAXDMDEC994MG1EM89&pf_rd_s=merchandised-search-15&pf_rd_t=101&qid=1601968662&ref=sr_pg_2'
+amazon_categories = [url_amazon_Electronics, url_amazon_ToysGames_AmazonExclusiveToys,
+                    url_amazon_ToysGames_ArtsCrafts, url_amazon_ToysGames_BabyToddlerToys, url_amazon_ToysGames_BuildingToys, 
+                    url_amazon_ToysGames_Dolls, url_amazon_DressUpPretendPlay,
+                    url_amazon_Games, url_amazon_vehicles, url_amazon_PreschoolToys,
+                    url_amazon_Puzzles, url_amazon_ToyCars, url_amazon_RideOns, url_amazon_SportsOutdoorPlay,
+                    url_amazon_SteamToys, url_amazon_StuffedAnimals]
+categories_name_amazon = ["Electronics", "ToysGames_AmazonExclusiveToys", "ToysGames_ArtsCrafts",
+                            "ToysGames_BabyToddlerToys", "ToysGames_BuildingToys", "ToysGames_Dolls",
+                            "DressUpPretendPlay", "amazon_Games", "vehicles", "PreschoolToys", "Puzzles",
+                            "ToyCars", "RideOns", "SportsOutdoorPlay", "SteamToys", "StuffedAnimals"]
+ASIN_SIZE = 10
+HREF_PREFIX = 'https://www.'
+SPACE_ASCII = 32
+DOT_ASCII = 46
+EQUAL_ASCII = 61
+QUESTING_MARK_ASCII = 63
+SEMI_COLON_ASCII = 59
+DOLLAR_ASCII = 36
+QUOTATION_MARKS_ASCII_DOUBLE = 34
+QUOTATION_MARKS_ASCII_SINGLE = 39
+STARS_SUFFIX = 'out of 5 stars'
+SPARE_CHARS_LIMIT = 5
+PRICE_ZONE = 100
+AMAZON_HREF_PREFIX = "https://www.amazon.com/"
+NAME_KEY_FULL_1 = '''span class="a-size-base-plus a-color-base a-text-normal" dir="auto">'''
+NAME_KEY_FULL_2 = '''span class="a-size-medium a-color-base a-text-normal" dir="auto">'''
+NAME_STOP_BUILD_KEY = '</span>'
+RATING_KEY = '''span aria-label="'''
+NAME_LIMIT = 500
+COMMA_ASCII = 44
+SPACE_ASCII = 32
+QUOTATION_MARKS_ASCII_DOUBLE = 34
+SPARE_CHARS_LIMIT = 5
+AMAZON_BRANDS = ['AmazonBasics', 'Amazon.com']
+MAX_NUMBER_OF_CHARS_FROM_IMG_TO_SRC = 200
+################    Ebay:
+MINIMUM_RATION_NAME_IN_HREF = 0.8
+#               MUST choose NEW (LH_ItemCondition=) and FREE SHIPPING (LH_FS=1) with PAGE 1 (_pgn=1)!!!            #
+urls_ebay_Dolls_and_Teddy_Bears = 'https://www.ebay.com/b/American-Girl-Dolls/95230/bn_16561431?LH_FS=1&LH_ItemCondition=1000&rt=nc&LH_FS=1&_pgn=1'
+urls_ebay_Diecast_And_Toy_Vehicles = 'https://il.ebay.com/b/Diecast-Toy-Vehicles/222/bn_1850842?LH_ItemCondition=3&rt=nc&LH_FS=1&_pgn=1'
+url_ebay_Electronics_Search = 'https://www.ebay.com/sch/i.html?_sacat=0&_udlo=&_udhi=&_ftrt=901&_ftrv=1&_sabdlo=&_sabdhi=&_samilow=&_samihi=&_sadis=15&_stpos=22747&_sop=12&_dmd=1&_fosrp=1&LH_ItemCondition=3&_nkw=electronics&LH_FS=1&_pgn=1'
+url_ebay_Toys_Search = 'https://www.ebay.com/sch/i.html?_sop=12&_sadis=15&_stpos=22747&_from=R40&_nkw=toys&_sacat=0&rt=nc&LH_ItemCondition=3&_ipg=200&LH_FS=1&_pgn=1'
+url_ebay_Games_Search = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=games&_sacat=0&LH_TitleDesc=0&_sop=12&_sadis=15&_stpos=22747&rt=nc&LH_ItemCondition=3&LH_FS=1&_pgn=1'
+url_ebay_Puzzles_Search = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=Puzzles&_sacat=0&LH_TitleDesc=0&_sop=12&_sadis=15&_stpos=22747&rt=nc&LH_ItemCondition=1000&LH_FS=1&_pgn=1'
+url_ebay_StuffedAnimals_Search = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=Stuffed+Animals&_sacat=0&LH_TitleDesc=0&_sop=12&_sadis=15&_stpos=22747&rt=nc&LH_ItemCondition=1000&LH_FS=1&_pgn=1'
+url_ebay_SteamToys_Search = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=Steam+Toys&_sacat=0&LH_TitleDesc=0&_sop=12&_sadis=15&_stpos=22747&rt=nc&LH_ItemCondition=1000&LH_FS=1&_pgn=1'
+url_ebay_Computers_Search = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=Computers&_sacat=3270&LH_TitleDesc=0&LH_ItemCondition=1000&rt=nc&LH_FS=1&_pgn=1'
+url_ebay_Headphones_Search = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=Headphones&_sacat=3270&LH_TitleDesc=0&LH_FS=1&rt=nc&LH_ItemCondition=1000&_pgn=1'
+url_ebay_Audio_Search = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=Audio&_sacat=3270&LH_TitleDesc=0&LH_FS=1&rt=nc&LH_ItemCondition=1000&_pgn=1'
+url_ebay_Speakers_Search = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=Speakers&_sacat=0&LH_ItemCondition=3&rt=nc&LH_FS=1&_pgn=1'
+url_ebay_Cameras_Search = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=Cameras&_sacat=0&LH_TitleDesc=0&LH_FS=1&rt=nc&LH_ItemCondition=3&_pgn=1'
+url_ebay_CarVideo_Search = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=Car+Video&_sacat=0&LH_TitleDesc=0&LH_FS=1&rt=nc&LH_ItemCondition=3&_pgn=1'
+ebay_categories = [urls_ebay_Dolls_and_Teddy_Bears, urls_ebay_Diecast_And_Toy_Vehicles,
+                     url_ebay_Electronics_Search, url_ebay_Toys_Search, url_ebay_Games_Search, url_ebay_Puzzles_Search,
+                     url_ebay_StuffedAnimals_Search, url_ebay_SteamToys_Search, url_ebay_Computers_Search, url_ebay_Headphones_Search,
+                     url_ebay_Audio_Search, url_ebay_Speakers_Search, url_ebay_Cameras_Search, url_ebay_CarVideo_Search]
+ebay_categories_names = ["American-Girl-Dolls", "Diecast-And-Toy-Vehicles", "Electronics", "Toys", "Games",
+                        "Puzzles", "StuffedAnimals", "SteamToys", "Electronics", "Electronics", "Electronics",
+                        "Electronics", "Electronics", "Electronics"]
+#                   General
+IMAGE_TYPES = ['.webp', '.jpg']
+# (amazon_pairs, ebay_pairs) ... (amazon_pairs, ebay_pairs)
+Electronics_Amazon_EBay = [["Electronics"], ["Electronics"]]
+Toys_Amazon_Ebay = [["ToysGames_AmazonExclusiveToys", "ToysGames_BabyToddlerToys", "ToysGames_ArtsCrafts", "ToysGames_BuildingToys", "ToysGames_Dolls", "DressUpPretendPlay", "PreschoolToys", "ToyCars", "RideOns", "SportsOutdoorPlay", "SteamToys", "StuffedAnimals", "vehicles"], 
+                    ["American-Girl-Dolls", "Diecast-And-Toy-Vehicles", "Toys", "StuffedAnimals", "SteamToys"]]
+Puzzles_Amazon_Ebay = [[url_amazon_Puzzles], [url_ebay_Puzzles_Search]]
+Games_Amazon_Ebay = [[url_amazon_Games], [url_ebay_Games_Search]]
+similar_categories_Amazon_Ebay = [Electronics_Amazon_EBay, Toys_Amazon_Ebay, Puzzles_Amazon_Ebay, Games_Amazon_Ebay]
+#                   Sorting parameters
+MIN_PRICE = 5.0
+MAX_PRICE = 150.0
+MAX_STARS = 5.0
+MIN_STARS = 4.0
+MIN_RATING = 100
+
+################    Initialization module  ################
+# initialize_Tools
+url_priceblink = 'https://chrome.google.com/webstore/detail/priceblink-coupons-and-pr/aoiidodopnnhiflaflbfeblnojefhigh'
+url_captcha_clicker = 'https://chrome.google.com/webstore/detail/im-not-robot-captcha-clic/ceipnlhmjohemhfpbjdgeigkababhmjc'
+# driverStarter
+TIME_DELAY_GET_NEW_URL = 15
+MAXIMUM_NUMBER_OF_REFRESHES = 5
+
+################    Mediation module  ################
+# By cross-referencing
+# Note - Must updates the values after building a function that check if two 
+#        products from diffrent sites are the same (By checking all the product
+#        in the gap table and)
+MINIMUM_VALUE_NAME_SIMILAR = 0.6 # # By Skyrocket Blume Doll
+MAXIMUM_VALUE_NAME_SIMILAR = 1.0
+MINIMUM_VALUE_IMAGE_NUMBER_OF_MATCHES = 100 # By Roku
+MAXIMUM_VALUE_IMAGE_AVARAGE_DISTANCES = 50.0 # By Roku
+# By PriceBlink
+PRICE_BLINK_IFRAME_ID = 'pb1_iframe'
+PRICE_BLINK_LIST_CLASS_NAME = 'dropdown-results-list'
+PRICE_BLINK_PRODUCT_CLASS_NAME = 'dropdown-list-item'
+PRICE_BLINK_TIME_TO_WAIT_FOR_PRICES_TO_LOAD = 1
+PRICE_BLINK_TIME_TO_WAIT_FOR_TOOLBAR_TO_LOAD = 15
+KEY_SORT_PERSONAL_PRODUCT_SITE = 'add to cart'
+
+################    ReCapcha module    ################
+# Reconnecting
+path = ["C:\\Program Files (x86)\\ExpressVPN\\expressvpn-ui\\ExpressVPN.exe"]
+VPN_DELAY_TIME = 1
+VPN_CONNECTING_TIME = 10
+VPN_LIST_OF_LOCATION = ["USA - New York", "USA - New York - 2", "USA - Washington", "USA - New Jersey - 1", "USA - New Jersey - 3",
+                        "USA - San Francisco", "USA - Chicago", "USA - Dallas", "USA - Miami", "USA - Miami - 2",
+                        "USA - Los Angeles", "USA - Los Angeles - 1", "USA - Los Angeles - 2", "USA - Los Angeles - 3",
+                        "USA - Los Angeles - 5", "USA - Seattle", "USA - Denver", "USA - Salt Lake City",
+                        "USA - Tampa - 1", "USA - Hollywood", "USA - Dallas - 2", "USA - Atlanta", "USA - Santa Monica"]
+
+# Capcha Handler
+CAPTCHA_PREFIX = 'https://www.google.com/sorry/index?continue='
+MODE_CLASSIFICATION = 0
+MODE_MEDIATION = 1
+
+################    SQLHandler      ################
+AMAZON_PRODUCTS_TABLE = 'AmazonProducts'
+EBAY_PRODUCTS_TABLE = 'EbayProducts'
+GAPS_AMAZON_EBAY_PRODUCTS_TABLE = 'GapsAmazonEbayProducts'
+PRODUCTS_DESTANCES_TABLE = 'ProductsDestances'
+STORE_PRODUCTS_TABLE = 'StoreProducts'
+USER_ORDER = 'UserOrder'
+
+################    Images pathes   ################
+IMAGE_PATH_AMAZON_NEXTPAGE = r'Images\Amazon_NextPage.png'
+IMAGE_PATH_AMAZON_NEXTPAGE_2 = r'Images\Amazon_NextPage2.png'
+
+################    __Main__        ################
+LARGE_FONT = ("Verdana", 12)
+SMALL_FONT = ("Verdana", 10)
+VERY_SMALL_FONT = ("Verdana", 8)
+
+################    Navigation  ################
+# Tools
+DEFAULT_HEIGHT = 1000
+SCROLL_TIME_LIMIT = 0.5
+SCROLL_INCREASE = 700
+MAX_HEIGHT = 5000
+MAX_NUMBER_OF_TRYING = 20
+LIMIT_NUMBER_OF_PAGE_TO_SEARCH = 200
+
+################    StoreUploading  ################
+EBAY_SHIPPING_KEY = 'logistics-cost'
+HYPHEN_ASCII = 45
+AMAZON_ASIN_SIZE = 10
+AMAZON_SITE = 'https://www.amazon.com/'
+EBAY_SITE = 'https://www.ebay.com/'
+EBAY_SELL_CENTER = 'https://bulksell.ebay.com/ws/eBayISAPI.dll?SingleList&forcetool=CUB'
+AMAZON_SEARCH_BAR_KEY = '//*[@id="f"]'
+STATUS_NOT_UPLOAD = 'WAITING_FOR_UPLOADING'
+STATUS_UPLOAD = 'UPLOADED'
+EBAY_SELLING_PRODUCT_SEARCH_BAR_KEY = 'w0-find-product-search-bar-search-field'
+EBAY_SELLING_PRODCUT_SEARCH_BAY_IFRAME = '//*[@id="findprod_iframe"]'
+EBAY_SELLING_PRODUCT_CONTINUE = '//*[@id="w0-find-product-2"]/div[2]/button'
+EBAY_SELLING_CREATE_A_LIST_UPC = '//*[@id="v4-6_msAnc"]'
+EBAY_SELLING_CREATE_A_LIST_UPC_OPTION_2 = '//*[@id="upc_menu_js_an_0"]'
+EBAY_SELLING_CREATE_A_LIST_CONDITION = '//*[@id="itemCondition"]'
+EBAY_SELLING_CREATE_A_LIST_IMPORT_IMAGE_FROM_WEB = '//*[@id="tm-topMsg"]/div[2]/div[1]/div[2]/div[2]/a[2]'
+EBAY_SELLING_CREATE_A_LIST_PHOTOS_IFRAME = 'uploader_iframe'
+EBAY_SELLING_CREATE_A_LIST_ENTER_IMAGE_URL = '//*[@id="cw-inpList"]/li/label/input'
+EBAY_SELLING_CREATE_A_LIST_IMPORT_IMAGE_BUTTON = '//*[@id="cw-urlForm"]/div[3]/div/a[1]'
+EBAY_SELLING_CREATE_A_LIST_BRAND = '//*[@id="Listing.Item.ItemSpecific[Brand]"]'
+EBAY_SELLING_CREATE_A_LIST_MPN = '//*[@id="Listing.Item.ItemSpecific[MPN]"]'
+EBAY_SELLING_CREATE_A_LIST_ITEM_DESCRIPTION_IFRAME = 'v4-29txtEdit_st'
+EBAY_SELLING_CREATE_A_LIST_ITEM_DESCRIPTION_TEXT = '/html/body'
+EBAY_SELLING_CREATE_A_LIST_SELLING_DETAILS = '//*[@id="format"]'
+EBAY_SELLING_CREATE_A_LIST_SELLING_DETAILS_PRICE = '//*[@id="binPrice"]'
+EBAY_SELLING_CREATE_A_LIST_SELLING_PAYPAL = '//*[@id="pmPayPal"]'
+EBAY_SELLING_CREATE_A_LIST_SELLING_RECEIVING_PAYMENT_EMAIL = '//*[@id="paypalEmail"]'
+EBAY_SELLING_CREATE_A_LIST_LIST_ITEM = '//*[@id="actionbar"]/input[1]'
+AD_GINO_TEAM_EMAIL = 'AD.Ginoteam@gmail.com'
